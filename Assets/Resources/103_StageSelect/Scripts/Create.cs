02f3fb0df2c81e34d6dd065cb530bbf83adb05ab
Tim <tjgowls1@naver.com>
2016-02-01 05:08:22 +0900

    Add_Stage_Scene
    메뉴화면이랑 스테이지 선택씬올림

---
+++ Assets/Resources/103_StageSelect/Scripts/Create.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Create : MonoBehaviour {//크래딧이나 옵션뛰우는 창
+
+    public void Pop()
+    {
+        transform.localPosition = new Vector3(0, 0, 1);
+    }
+    public void Destory()
+    {
+        transform.localPosition = new Vector3(4000, 0, 1);
+    }
+
+}
